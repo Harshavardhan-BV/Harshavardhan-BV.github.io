50939347ea12c9e528c5babc1778da055cc200ae
Harshavardhan-BV <harshavardhan.bv@students.iiserpune.ac.in>
2022-04-03 15:47:20 +0530

    fixed links for contact

--- assets/js/cloak.js
+++ assets/js/cloak.js
@@ -1,30 +1,38 @@
-var f_name ='harshavardhan',
-    l_name ='bv',
-    F_name ='Harshavardhan',
-    L_name ='BV',
-    alter_ego = 'rip-deadman-rip',
-    mail = 'live.com',
-    isd = '+91',
-    ph_front = '944'
-    ph_mid = '580',
-    ph_end = '2699';
+var f_name='harshavardhan',l_name='bv',F_name='Harshavardhan',L_name='BV',alter_ego='rip-deadman-rip',leet_ego='d3dm4n',mail='live.com',isd='+91',ph_front='944',ph_mid='580',ph_end = '2699',d_code='3807';
 
-function ePost(){
-    mail_link = 'mailto:'+l_name+'_'+f_name+'@'+mail;
-    window.open(mail_link,"_self");
+function hyperClick(type){
+    switch (type) {
+        case 'email': link='mailto:'+l_name+'_'+f_name+'@'+mail;target="_self";
+        break;
+        case 'phone': link='tel:'+isd+'-'+ph_front+'-'+ph_mid+'-'+ph_end;target="_self";
+        break;
+        case 'signal': link='https://signal.me/#p/+'+isd+ph_front+ph_mid+ph_end;;target="_blank";
+        break;
+        case 'matrix': link='https://matrix.to/#/@'+alter_ego+':matrix.org';target="_blank";
+        break;
+        case 'telegram': link='https://t.me/'+F_name+'_'+L_name;target="_blank";
+        break;
+        case 'discord': link='https://discord.com/app';target="_blank";
+        break;
+        default: link='#';target="_self";
+    }
+    window.open(link, target);
 }
 
-function grahamBell(){
-    phone_link='tel:'+isd+'-'+ph_front+'-'+ph_mid+'-'+ph_end;
-    window.open(phone_link,"_self");
-}
-
-function neoMorpheus(){
-    mat_link = 'https://matrix.to/#/@'+alter_ego+':matrix.org';
-    window.open(mat_link, "_blank");
-}
-
-function morseCode(){
-    tele_link = 'https://t.me/'+F_name+L_name;
-    window.open(tele_link, "_blank");
+function hyperText(type){
+    switch (type) {
+        case 'email': text=l_name+'_'+f_name+'@'+mail;
+        break;
+        case 'phone': text=isd+'-'+ph_front+'-'+ph_mid+'-'+ph_end;
+        break;
+        case 'signal': text=isd+'-'+ph_front+'-'+ph_mid+'-'+ph_end;
+        break;
+        case 'matrix': text=alter_ego;
+        break;
+        case 'telegram': text=F_name+L_name;
+        break;
+        case 'discord': text=leet_ego+'#'+d_code;
+        break;
+        default: text='';
+    }
 }
